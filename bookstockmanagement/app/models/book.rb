acad318ad24c9c4b44c1166785a3fc0e7e4395fe
Medha <13552797+Harimedha@users.noreply.github.com>
2023-01-16 16:43:18 +0530

    Adding swagger api for documentation

--- bookstockmanagement/app/models/book.rb
+++ bookstockmanagement/app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
 
-    validates :bookName, :bookAuthor, presence: true, length: {in: 1..100}   
+    validates :bookName, :bookAuthor, presence: true, length: {in: 1..100}  , uniqueness: true
     validates :bookQuantity, numericality: {only_integer: true}, allow_blank: true
 
 end
